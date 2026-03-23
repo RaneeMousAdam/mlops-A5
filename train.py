bcfd50213a856c7489d2b790020ad6e4bf3fbe4f
RaneeMousAdam <s-raneem.adam@zewailcity.edu.eg>
2026-03-23 22:28:26 +0200

    Fix MLflow tracking URI issue

--- train.py
+++ train.py
@@ -5,8 +5,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
-
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "file:./mlruns"))
 X, y = load_iris(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
